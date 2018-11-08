7def92fac5325de135f8674b550a41898921db39
Ashlee Richards <ashlee.richards@gmail.com>
2018-11-08 11:43:21 -0500

    adds behind the scenes page with baguette gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+const gallery = baguetteBox.run('.tz-gallery');
+baguetteBox.show(index, lightbox[0]);
+
 function myFunction() {
     var x = document.getElementById("myTopNav");
     if (x.className === "topnav") {
